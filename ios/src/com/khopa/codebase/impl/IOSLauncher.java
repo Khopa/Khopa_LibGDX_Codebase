904aabfe4c1acb45203bc0540cc90f734914f651
Khopa <clemguip@gmail.com>
2014-10-21 09:34:31 -0400

    Generated libgdx template project
    Result of the libgdx setup

---
+++ ios/src/com/khopa/codebase/impl/IOSLauncher.java
@@ -0,0 +1,22 @@
+package com.khopa.codebase.impl;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import com.khopa.codebase.impl.ExampleGame;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new ExampleGame(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}
